892857a7aa8bf9aa051d4195f430abb2cc8c8052
stanclik <mmstanclik@gmail.com>
2021-10-28 10:16:32 -0400

    Add caching to getHistoricalPrices

--- src/main/java/org/galatea/starter/domain/IexHistoricalPrice.java
+++ src/main/java/org/galatea/starter/domain/IexHistoricalPrice.java
@@ -1,19 +1,35 @@
 package org.galatea.starter.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Data
 @Builder
+@Entity
 public class IexHistoricalPrice {
 
-  private BigDecimal close;
-  private BigDecimal high;
-  private BigDecimal low;
-  private BigDecimal open;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @JsonIgnore
+  private Long uid;
+
   private String symbol;
-  private Integer volume;
   private String date;
+  private BigDecimal open;
+  private BigDecimal low;
+  private BigDecimal high;
+  private BigDecimal close;
+  private Integer volume;
 
 }
